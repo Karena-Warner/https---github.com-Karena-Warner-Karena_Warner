e54c84fa98bd1332c772f69f0d52901581cbfbad
Karena-Warner <kwarner234@me.com>
2024-09-15 14:55:27 +0100

    First commit

---
+++ Karenas_project/5_optimal_skills.sql
@@ -0,0 +1,55 @@
+/*
+Answer: What are the most optimal skills to learn (ask it's in high demand and a high_paying skill)?
+-Identify skills in high demand and associated with high average salaries for Data Analyst roles
+-concentrates on remote positions with specified salaries
+-Why? Targets skills that offer job security (high demand) and finanacial benefits (high salaries), offering strategic insights for career development in data analysis
+*/
+
+WITH skills_demand AS (
+SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        Count(skills_job_dim.job_id) As demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE job_title_short ='Data Analyst' 
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE 
+    Group by skills_dim.skill_id
+),
+average_salary AS (
+SELECT 
+        skills_job_dim.skill_id,
+
+        ROUND(AVG(job_postings_fact.salary_year_avg),2) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE job_title_short ='Data Analyst' 
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
+Group by skills_job_dim.skill_id
+)
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+
+FROM 
+    skills_demand
+
+INNER JOIN
+    average_salary ON skills_demand.skill_id = average_salary.skill_id
+
+WHERE demand_count > 10
+
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+    
+
+Limit 25;
+
+
